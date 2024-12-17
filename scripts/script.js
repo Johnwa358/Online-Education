b792bad0a537d15e47c2b4697218b2e0f17bd424
Johnwa358 <gudduwa@gmail.com>
2024-12-17 13:46:27 +0530

    second commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// const menu = document.getElementById("menu");
+// const rightSection = document.querySelector(".rightSection");
+
+
+// menu.addEventListener(click, function() {
+//     rightSection.classList.toggle("open");
+// });
+
+
+
+
